c05caa919f4b07a08292dfde49478ef0b40cf5fc
Natalie <natalie.todd15@gmail.com>
2018-08-12 13:33:19 -0600

    getting setup ready, added a Joblist component

--- intro-vue/src/App.vue
+++ intro-vue/src/App.vue
@@ -1,23 +1,20 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div id='app'>
+<!-- Add in component, ex: <Todo /> -->
+<Joblist />
   </div>
 </template>
 
 <script>
+// import component from component path
+import Joblist from "./components/Joblist.vue";
 export default {
-  name: 'App',
+  name: "App",
+  components: {
+    Joblist
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
