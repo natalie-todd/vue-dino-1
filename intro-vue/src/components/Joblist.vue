270f8228913bb7035e240c726679f49556c31bd6
Natalie <natalie.todd15@gmail.com>
2018-08-12 14:07:20 -0600

    not sure why list isnt showing up

--- intro-vue/src/components/Joblist.vue
+++ intro-vue/src/components/Joblist.vue
@@ -8,6 +8,24 @@
 </template>
 
 <script>
+export default {
+    data() {
+        return {
+            jobs: [],
+        };
+    },
+    methods: {
+        listingJobs() {
+            const JOB_JSON = './listings.json';
+            fetch(JOB_JSON)
+            .then(res => res.json())
+            .then (result => {
+                this.jobs = result
+                console.log(result)
+            })
+        }
+    }
+}
 </script>
 
 <style>
